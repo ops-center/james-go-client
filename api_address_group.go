bdbaeca817d8150abd91897a6f8e93bf55430bd5
Imtiaz Uddin <imtiaz@appscode.com>
2024-02-06 11:49:34 +0600

    Add delete group and delete groups api
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>

--- api_address_group.go
+++ api_address_group.go
@@ -423,3 +423,191 @@ func (a *AddressGroupAPIService) RemoveMemberExecute(r ApiRemoveMemberRequest) (
 
 	return localVarHTTPResponse, nil
 }
+
+
+type ApiDeleteGroupRequest struct {
+	ctx context.Context
+	ApiService *AddressGroupAPIService
+	groupAddress *string
+}
+
+func (r ApiDeleteGroupRequest) Execute() (*http.Response, error) {
+	return r.ApiService.DeleteGroupExecute(r)
+}
+
+/*
+DeleteGroup Remove a group member
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param groupAddress Group mail address
+ @return ApiDeleteGroupRequest
+*/
+func (a *AddressGroupAPIService) DeleteGroup(ctx context.Context, groupAddress string) ApiDeleteGroupRequest {
+	return ApiDeleteGroupRequest{
+		ApiService: a,
+		ctx: ctx,
+		groupAddress: &groupAddress,
+	}
+}
+
+// Execute executes the request
+func (a *AddressGroupAPIService) DeleteGroupExecute(r ApiDeleteGroupRequest) (*http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodDelete
+		localVarPostBody     interface{}
+		formFiles            []formFile
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "AddressGroupAPIService.DeleteGroup")
+	if err != nil {
+		return nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	if r.groupAddress == nil {
+		return nil, reportError("groupAddress is requiered and must be specified")
+	}
+	localVarPath := localBasePath + "/address/groups/" + *r.groupAddress
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarHTTPResponse, newErr
+	}
+
+	return localVarHTTPResponse, nil
+}
+
+type ApiDeleteGroupsRequest struct {
+	ctx context.Context
+	ApiService *AddressGroupAPIService
+	groupAddresses []string
+}
+
+func (r ApiDeleteGroupsRequest) Execute() (*http.Response, error) {
+	return r.ApiService.DeleteGroupsExecute(r)
+}
+
+/*
+DeleteGroups Deletes a list of grups
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param groupAddresses is a list of Group mail address
+ @return ApiDeleteGroupsRequest
+*/
+func (a *AddressGroupAPIService) DeleteGroups(ctx context.Context, groupAddress []string) ApiDeleteGroupsRequest {
+	return ApiDeleteGroupsRequest{
+		ApiService: a,
+		ctx: ctx,
+		groupAddresses: groupAddress,
+	}
+}
+
+// Execute executes the request
+func (a *AddressGroupAPIService) DeleteGroupsExecute(r ApiDeleteGroupsRequest) (*http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodDelete
+		localVarPostBody     interface{}
+		formFiles            []formFile
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "AddressGroupAPIService.DeleteGroups")
+	if err != nil {
+		return nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	if r.groupAddresses == nil {
+		return nil, reportError("groupAddresses is requiered and must be specified")
+	}
+	localVarPath := localBasePath + "/address/groups"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+
+	// body params
+	localVarPostBody = r.groupAddresses
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarHTTPResponse, newErr
+	}
+
+	return localVarHTTPResponse, nil
+}
