f58d16d5e46b0134905421f607dc4dbb1ca913f1
Imtiaz Uddin <imtiaz@appscode.com>
2024-02-12 17:08:36 +0600

    Add create group api
    Signed-off-by: Imtiaz Uddin <imtiaz@appscode.com>

--- api_address_group.go
+++ api_address_group.go
@@ -436,7 +436,7 @@ func (r ApiDeleteGroupRequest) Execute() (*http.Response, error) {
 }
 
 /*
-DeleteGroup Remove a group member
+DeleteGroup deletes a group
 
  @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  @param groupAddress Group mail address
@@ -584,6 +584,98 @@ func (a *AddressGroupAPIService) DeleteGroupsExecute(r ApiDeleteGroupsRequest) (
 
 	// body params
 	localVarPostBody = r.groupAddresses
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarHTTPResponse, newErr
+	}
+
+	return localVarHTTPResponse, nil
+}
+
+type ApiCreateGroupRequest struct {
+	ctx context.Context
+	ApiService *AddressGroupAPIService
+	groupAddress *string
+}
+
+func (r ApiCreateGroupRequest) Execute() (*http.Response, error) {
+	return r.ApiService.CreateGroupExecute(r)
+}
+
+/*
+CreateGroup creates a group with dummy user
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param groupAddress Group mail address
+ @return ApiCreateGroupRequest
+*/
+func (a *AddressGroupAPIService) CreateGroup(ctx context.Context, groupAddress string) ApiCreateGroupRequest {
+	return ApiCreateGroupRequest{
+		ApiService: a,
+		ctx: ctx,
+		groupAddress: &groupAddress,
+	}
+}
+
+// Execute executes the request
+func (a *AddressGroupAPIService) CreateGroupExecute(r ApiCreateGroupRequest) (*http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodPost
+		localVarPostBody     interface{}
+		formFiles            []formFile
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "AddressGroupAPIService.CreateGroup")
+	if err != nil {
+		return nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	if r.groupAddress == nil {
+		return nil, reportError("groupAddress is requiered and must be specified")
+	}
+	localVarPath := localBasePath + "/address/groups/" + *r.groupAddress
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
 	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
 	if err != nil {
 		return nil, err
