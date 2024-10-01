9a4aabfb348108a3e0c92be285a3f446e25f55db
samiulsami <33352407+samiulsami@users.noreply.github.com>
2024-10-01 18:10:38 +0600

    Reverse group/member hierarchy (#29)
    Signed-off-by: sami <sami@appscode.com>

--- inbox/helpers_test.go
+++ inbox/helpers_test.go
@@ -1,50 +1,94 @@
 package inbox
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestGetGroupAndAssociatedMemberIdentifier(t *testing.T) {
+
 	jamesIdentifier := ObjectIdentifier{
-		ObjectName:     "workload",
+		ObjectName:     "database",
 		ObjectUniqueID: "1",
 		ObjectType:     DbType,
 		IsGroupType:    true,
 		ParentObject: &ObjectIdentifier{
 			ObjectName:     "namespace",
 			ObjectUniqueID: "2",
-			ObjectType:     DbType,
+			ObjectType:     NamespaceType,
 			IsGroupType:    true,
 			ParentObject: &ObjectIdentifier{
 				ObjectName:     "cluster",
 				ObjectUniqueID: "3",
-				ObjectType:     DbType,
+				ObjectType:     ClusterType,
 				IsGroupType:    true,
 			},
 		},
 	}
 
-	results, err := getGroupAndAssociatedMemberIdentifier(jamesIdentifier, nil)
+	results, err := GetGroupAndAssociatedMemberIdentifier(jamesIdentifier)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	if len(results) != 2 {
-		t.Error(fmt.Errorf("length of result doesn't match: expected: %v, got: %v", 3, len(results)))
-		return
+	expected := []GroupAndAssociatedMemberIdentifier{
+		{
+			Group: ObjectIdentifier{
+				ObjectName:     "database",
+				ObjectUniqueID: "1",
+				ObjectType:     DbType,
+				IsGroupType:    true,
+				ParentObject: &ObjectIdentifier{
+					ObjectName:     "namespace",
+					ObjectUniqueID: "2",
+					ObjectType:     NamespaceType,
+					IsGroupType:    true,
+					ParentObject: &ObjectIdentifier{
+						ObjectName:     "cluster",
+						ObjectUniqueID: "3",
+						ObjectType:     ClusterType,
+						IsGroupType:    true,
+					},
+				},
+			},
+			Member: ObjectIdentifier{
+				ObjectName:     "namespace",
+				ObjectUniqueID: "2",
+				ObjectType:     NamespaceType,
+				IsGroupType:    true,
+				ParentObject: &ObjectIdentifier{
+					ObjectName:     "cluster",
+					ObjectUniqueID: "3",
+					ObjectType:     ClusterType,
+					IsGroupType:    true,
+				},
+			},
+		},
+		{
+			Group: ObjectIdentifier{
+				ObjectName:     "namespace",
+				ObjectUniqueID: "2",
+				ObjectType:     NamespaceType,
+				IsGroupType:    true,
+				ParentObject: &ObjectIdentifier{
+					ObjectName:     "cluster",
+					ObjectUniqueID: "3",
+					ObjectType:     ClusterType,
+					IsGroupType:    true,
+				},
+			},
+			Member: ObjectIdentifier{
+				ObjectName:     "cluster",
+				ObjectUniqueID: "3",
+				ObjectType:     ClusterType,
+				IsGroupType:    true,
+			},
+		},
 	}
 
-	for _, result := range results {
-		fmt.Printf("group: %s, member: %s\n", result.Group.ObjectName, result.Member.ObjectName)
-		if !result.Member.HasParentObject() {
-			t.Errorf("member %s does not have parent object", result.Member.ObjectName)
-			return
-		}
-		if result.Member.ParentObject.GetUniqueID() != result.Group.GetUniqueID() {
-			t.Errorf("member parent reference does not point to group object")
-			return
-		}
+	if diff := cmp.Diff(results, expected); len(diff) != 0 {
+		t.Errorf("difference between expected output and result:\n%s", diff)
 	}
 }
